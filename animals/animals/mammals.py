357aefbc9074a28bf534e6dc30de4619dff7a133
Blue Zephyr <blue.zephyr.git@gmail.com>
2020-10-03 18:50:34 +0200

    Add refresh method to MammalsSet class

--- animals/animals/mammals.py
+++ animals/animals/mammals.py
@@ -1,2 +1,13 @@
 class MammalsSet:
-    pass
+    def __init__(self, animals_db):
+        """
+        The mammals set object needs access to an object of the type animals.animal_db.Animals
+        class. This is given as input when the mammals set is initialized.
+        """
+        self.animals_db = animals_db
+
+    def refresh(self):
+        """
+        Request the database to refresh the set of mammals.
+        """
+        self.animals_db.animals()
--- animals/tests/test_mammals.py
+++ animals/tests/test_mammals.py
@@ -1,10 +1,17 @@
 import unittest
+from unittest.mock import Mock
 from animals.animals.mammals import MammalsSet
 
 
 class TestMammalsSet(unittest.TestCase):
     def test_refresh_list_generates_call_to_database(self):
-        mammals = MammalsSet()
+        """
+        Make sure that the set of mammals is updated with a request to the database
+        when the refresh message is sent.
+        """
+        db = Mock()
+        MammalsSet(db).refresh()
+        db.animals.assert_called()
 
 
 if __name__ == "__main__":
